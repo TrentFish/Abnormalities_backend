52dfd7eea7e731a881bbd77cce48617ba2b15eb0
fishyboi@MSI <fishyboi@MSI>
2023-09-21 00:10:07 -0400

    delete put post

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const pg = require('pg');
 const client = new pg.Client("postgres://localhost/abnormalities_backend_db");
 
 app.use(morgan('dev'));
+app.use(express.json());
 
 app.get('/api/abnormalities', async(req, res, next) => {
     try {
-        const SQL = `SELECT * FROM abnormalities`;
+        const SQL = `
+        SELECT *
+        FROM abnormalities
+        `;
 
         const response = await client.query(SQL);
         console.log(response.rows);
@@ -30,6 +34,55 @@ app.get('/api/abnormalities/:id', async(req, res, next) => {
         const response = await client.query(SQL,[req.params.id]);
         console.log(response.rows[0]);
 
+        res.send(response.rows);
+    }catch (error) {
+        next(error);
+    }
+});
+
+app.delete('/api/abnormalities/:id', async(req, res, next) => {
+    try {
+        const SQL = `
+        DELETE 
+        FROM abnormalities
+        WHERE id = $1
+        `;
+
+        const response = await client.query(SQL,[req.params.id]);
+        console.log(response.rows);
+
+        res.send(response.rows);
+    }catch (error) {
+        next(error);
+    }
+});
+
+app.post('/api/abnormalities', async(req, res, next) => {
+    const body = req.body;
+    try {
+        const SQL = `
+        INSERT INTO abnormalities(name, risk, ego)
+        VALUES($1, $2, $3)
+        RETURNING *
+        `;
+
+        const response = await client.query(SQL, [body.name, body.risk, body.ego]);
+
+    }catch (error) {
+        next(error);
+    }
+});
+
+app.put('/api/abnormalities/:id', async(req, res, next) => {
+    try {
+        const SQL = `
+        UPDATE abnormalities
+        SET name = $1, risk = $2, ego = $3
+        WHERE id = $4
+        RETURNING *
+        `;
+
+        const response = await client.query(SQL, [body.name, body.risk, body.ego]);
         res.send(response.rows);
     }catch (error) {
         next(error);
