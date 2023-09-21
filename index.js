539f762cf231e428475154b7137bfffa03ebf1ea
fishyboi@MSI <fishyboi@MSI>
2023-09-20 23:38:42 -0400

    singled out

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const pg = require('pg');
 const client = new pg.Client("postgres://localhost/abnormalities_backend_db");
 
@@ -7,10 +8,27 @@ app.use(morgan('dev'));
 
 app.get('/api/abnormalities', async(req, res, next) => {
     try {
-        const SQL = `SELECT * FROM abnormalities;`;
+        const SQL = `SELECT * FROM abnormalities`;
 
         const response = await client.query(SQL);
         console.log(response.rows);
+
+        res.send(response.rows);
+    }catch (error) {
+        next(error);
+    }
+});
+
+app.get('/api/abnormalities/:id', async(req, res, next) => {
+    try {
+        const SQL = `
+        SELECT * 
+        FROM abnormalities
+        WHERE id = $1
+        `;
+
+        const response = await client.query(SQL,[req.params.id]);
+        console.log(response.rows[0]);
 
         res.send(response.rows);
     }catch (error) {
@@ -29,11 +47,11 @@ const start = async() => {
             risk VARCHAR(100),
             ego VARCHAR(100)
         );
-        INSERT INTO flavors(name, risk, ego) VALUES ('Fairy Festival', 'ZAYIN', 'Wingbeat');
-        INSERT INTO flavors(name, risk, ego) VALUES ('Spider Bud', 'TETH', 'Red Eyes');
-        INSERT INTO flavors(name, risk, ego) VALUES ('Funeral of the Dead Butterflies', 'HE', 'Solemn Lament');
-        INSERT INTO flavors(name, risk, ego) VALUES ('Queen of Hatred', 'WAW', 'In the Name of Love and Hate');
-        INSERT INTO flavors(name, risk, ego) VALUES ('Nothing There', 'ALEPH', 'Mimicry');
+        INSERT INTO abnormalities(name, risk, ego) VALUES ('Fairy Festival', 'ZAYIN', 'Wingbeat');
+        INSERT INTO abnormalities(name, risk, ego) VALUES ('Spider Bud', 'TETH', 'Red Eyes');
+        INSERT INTO abnormalities(name, risk, ego) VALUES ('Funeral of the Dead Butterflies', 'HE', 'Solemn Lament');
+        INSERT INTO abnormalities(name, risk, ego) VALUES ('Queen of Hatred', 'WAW', 'In the Name of Love and Hate');
+        INSERT INTO abnormalities(name, risk, ego) VALUES ('Nothing There', 'ALEPH', 'Mimicry');
     `;
 
     await client.query(SQL);
